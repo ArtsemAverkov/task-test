0511edfa1135a32564511ab746713cd925d08b56
ArtsemAverkov <102361520+ArtsemAverkov@users.noreply.github.com>
2023-10-30 16:20:22 +0200

    add: implementation InMemoryProductRepository

--- src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
+++ src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
@@ -3,29 +3,40 @@ package ru.clevertec.product.repository.impl;
 import ru.clevertec.product.entity.Product;
 import ru.clevertec.product.repository.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public class InMemoryProductRepository implements ProductRepository {
 
+    private List<Product> products = new ArrayList<>();
+
     @Override
     public Optional<Product> findById(UUID uuid) {
-        return Optional.empty();
+        Optional<Product> product = products.stream()
+                .filter(p -> p.getUuid().equals(uuid))
+                .findFirst();
+        return product;
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return products;
     }
 
     @Override
     public Product save(Product product) {
-        return null;
+        UUID uuid = UUID.randomUUID();
+        product.setUuid(uuid);
+        products.add(product);
+        return product;
     }
 
     @Override
     public void delete(UUID uuid) {
-
+        products.removeIf(product -> product.getUuid().equals(uuid));
     }
+}
+
 }
